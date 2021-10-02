6f1930b9985e97209070cdafc38604e4654b137a
fohlle <patte2000@hotmail.com>
2021-10-02 16:26:10 +0200

    setup done

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./style.css"
+import { greet } from "./test.js"
+
+console.log("hi");
+
+greet("anna");
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: './src/index.js',
+    test: "./src/test.js"
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+};
